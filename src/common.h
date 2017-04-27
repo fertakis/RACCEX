709f8bf56ae08b2d06daeaa0766478d76a59a98b
Konstantinos Fertakis <fertakis@gmail.com>
2017-04-27 02:03:31 -0700

    Initial commit with basic implementation for server client sockets communication

---
+++ src/common.h
@@ -0,0 +1,19 @@
+/*
+ * common.h
+ *
+ * Remote Intel PHI Execution
+ *
+ * Konstantinos Fertakis <kfertak@cslab.ece.ntua.gr>
+ */
+
+#ifndef _COMMON_H
+#define _COMMON_H
+
+/* Compile-time options */
+#define TCP_PORT    35001
+#define TCP_BACKLOG 5
+
+#define HELLO_THERE "Hello there!"
+
+#endif /* _COMMON_H */
+
