d4e6df1b1e46a6db88a9e48530d11e59e23494ad
Konstantinos <kfertak@cslab.ece.ntua.gr>
2017-12-24 20:00:45 +0200

    server cmd process refactoring

--- src/process.c
+++ src/process.c
@@ -67,7 +67,7 @@ int exec_scif_close(scif_epd_t endp, client_node **cur_client)
 int exec_scif_bind(scif_epd_t endp, uint16_t pn, int *portno)
 {
 	int ret; 
-	if((int_res = scif_bind((scif_epd_t)cmd->int_args[0], (uint16_t)cmd->int_args[1])) < 0)
+	if((*portno = scif_bind((scif_epd_t)cmd->int_args[0], (uint16_t)cmd->int_args[1])) < 0)
 	{
 		perror("scif_bind");
 		ret = SCIF_BIND_FAIL;
@@ -94,6 +94,256 @@ int exec_scif_listen(scif_epd_t endp, int backlog)
 	}
 	return ret;
 }
+
+int exec_scif_connect(scif_epd_t endp, struct scif_portID *dst, int *portID)
+{
+	int ret; 
+	if((*portID = scif_connect(endp, dst)) < 0)
+	{
+		perror("scif_connect");
+		ret = SCIF_CONNECT_FAIL;
+	}
+	else 	
+		ret = SCIF_SUCCESS;
+	
+	return ret;
+}
+
+int exec_scif_accept(scif_epd_t endp, struct scif_portID *peer, scif_epd_t *newepd, int flags)
+{
+	int ret;
+	if(scif_accept(endp, peer, newepd, flags) < 0)
+	{
+		perror("scif_accept");
+		ret = SCIF_ACCEPT_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+	return ret; 
+}
+
+int exec_scif_send(scif_epd_t endp, void *msg, int len, int flags, int *send_count)
+{
+	int ret;
+	if((*send_count = scif_send(endp, msg, len, flags)) < 0)
+	{
+		perror("scif_send");
+		ret = SCIF_SEND_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+	return ret; 
+}
+
+int exec_scif_recv(scif_epd_t endp, void *msg, int len, int flags, int *read_count)
+{
+	int ret; 
+	
+	if((*read_count = scif_recv(endp, msg, len, flags)) < 0)
+	{
+		perror("scif_recv");
+		ret = SCIF_RECV_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+	return ret;
+}
+
+int exec_scif_register(scif_epd_t endp, void *addr, size_t len, off_t offset, int prot_flags, int map_flags, off_t *resulted_off_t)
+{
+	int ret; 
+	
+	if((*resulted_off_t = scif_register(endp, addr, len, offset, prot_flags, map_flags)) < 0)
+	{
+		perror("scif_register");
+		ret = SCIF_REGISTER_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+	return ret;
+}
+
+int exec_scif_unregister(scif_epd_t endp, off_t offset, size_t len)
+{
+	int ret; 
+	
+	if(scif_unregister(endp, offset, len) < 0)
+	{
+		perror("scif_unregister");
+		ret = SCIF_UNREGISTER_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+	return ret;
+}
+
+int exec_scif_unregister(scif_epd_t endp, off_t offset, size_t len)
+{
+	int ret; 
+	
+	if(scif_unregister(endp, offset, len) < 0)
+	{
+		perror("scif_unregister");
+		ret = SCIF_UNREGISTER_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+	return ret;
+}
+
+int exec_scif_mmap(void *addr, size_t len, int prot_flags, int map_flags, scif_epd_t epd, 
+		off_t offset, void *result)
+{
+	int ret = -2;
+
+	//TODO: To be implemented...
+
+	return ret;
+}
+
+int exec_scif_munmap(void *addr, size_t len)
+{
+	int ret = -2;
+
+	//TODO: To be implemented...
+
+	return ret;
+}
+
+int exec_scif_readfrom(scif_epd_t epd, off_t loffset, size_t len, off_t roffset, int rma_flags)
+{
+	int ret; 
+	
+	if(scif_readfrom(epd, loffset, len, roffset, rma_flags) < 0)
+	{
+		perror("scif_readfrom");
+		ret = SCIF_READFROM_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+
+	return ret;
+}
+
+int exec_scif_writeto(scif_epd_t epd, off_t loffset, size_t len, off_t roffset, int rma_flags)
+{
+	int ret; 
+	
+	if(scif_writeto(epd, loffset, len, roffset, rma_flags) < 0)
+	{
+		perror("scif_writeto");
+		ret = SCIF_WRITETO_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+
+	return ret;
+}
+
+int exec_scif_vreadfrom(scif_epd_t epd, void *addr, size_t len, off_t offset, int rma_flags)
+{
+	int ret; 
+	
+	if(scif_vreadfrom(epd, loffset, len, roffset, rma_flags) < 0)
+	{
+		perror("scif_vreadfrom");
+		ret = SCIF_VREADFROM_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+
+	return ret;
+}
+
+int exec_scif_vwriteto(scif_epd_t epd, void *addr, size_t len, off_t offset, int rma_flags)
+{
+	int ret; 
+	
+	if(scif_vwriteto(epd, loffset, len, roffset, rma_flags) < 0)
+	{
+		perror("scif_vwriteto");
+		ret = SCIF_VWRITETO_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+
+	return ret;
+}
+
+int exec_scif_fence_mark(scif_epd_t epd, int flags, int *mark)
+{
+	int ret; 
+	
+	if(scif_fence_mark(epd, flags, mark) < 0)
+	{
+		perror("scif_fence_mark");
+		ret = SCIF_FENCE_MARK_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+
+	return ret;
+}
+
+int exec_scif_fence_wait(scif_epd_t epd, int mark)
+{
+	int ret; 
+	
+	if(scif_fence_wait(epd, mark) < 0)
+	{
+		perror("scif_fence_wait");
+		ret = SCIF_FENCE_WAIT_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+
+	return ret;
+}
+
+int exec_scif_fence_signal(scif_epd_t epd, off_t loff, uint64_t lval, off_t roff, uint64_t rval,
+			int flags)
+{
+	int ret; 
+	
+	if(scif_fence_signal(epd, loff, lval, roff, rval, flags) < 0)
+	{
+		perror("scif_fence_signal");
+		ret = SCIF_FENCE_SIGNAL_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+
+	return ret;
+}
+
+int exec_scif_get_nodeIDs(uint16_t *nodes, int len, uint16_t *self, int *online_nodes)
+{
+	int ret; 
+
+	if((*online_nodes = scif_get_nodeIDs(nodes, len, self)) < 0)
+	{
+		perror("scif_get_nodeIDs");
+		ret = SCIF_GET_NODEIDS_FAIL;
+	}
+	else
+		ret = SCIF_SUCCESS;
+	return ret; 
+}
+
+int exec_scif_poll(struct scif_pollepd *epds, unsigned int nepds, long timeout, int *poll_res)
+{
+	int ret;
+
+	if((*poll_res = scif_poll(epds, nepds, timeout)) < 0 )
+	{
+		perror("scif_poll");
+		ret = SCIF_POLL_FAILED;
+	}
+	else
+		ret = SCIF_SUCCESS;
+	return ret;
+}
+
 int process_phi_cmd(void **result, void *cmd_ptr, client_node  **cur_client) {
 	int phi_result = 0, *int_res, int_res_count, uint_res_cout,arg_count = 1;
 	PhiCmd *cmd = cmd_ptr;
@@ -109,13 +359,13 @@ int process_phi_cmd(void **result, void *cmd_ptr, client_node  **cur_client) {
 			//int_res = scif_get_driver_version();
 			break;
 		case OPEN:
-				printf("Executing scif_open() ... \n");
-				arg_count++;
-				int_res = malloc_safe(sizeof(int));
-				int_res_count = 1;
-				phi_result = exec_scif_open(int_res, cur_client);
+			printf("Executing scif_open() ... \n");
+			arg_count++;
+			int_res = malloc_safe(sizeof(int));
+			int_res_count = 1;
+			phi_result = exec_scif_open(int_res, cur_client);
 
-				break;
+			break;
 		case CLOSE:
 			printf("Executing scif_close() ... \n");
 			//TODO: scif_close call goes here...
@@ -142,22 +392,12 @@ int process_phi_cmd(void **result, void *cmd_ptr, client_node  **cur_client) {
 		case CONNECT:
 			printf("Executing scif_connect() ... \n");
 			//TODO: scif_connect call goes here...
-			struct scif_portID *dst;
-			dst = (struct scif_portID *)cmd->extra_args[0].data;
-			printf("remote scif_portID: node=%d and port=%d \n", dst->node, dst->port);
+			
+			arg_count++:
+			int_res = malloc_safe(sizeof(int));
+			int_res_count = 1;
 
-			if((int_res = scif_connect((scif_epd_t)cmd->int_args[0], dst)) < 0)
-			{
-				perror("scif_connect");
-				phi_result = SCIF_CONNECT_FAIL;
-			}
-			else 
-			{	
-				phi_result = SCIF_SUCCESS;
-				res_type = INT;
-			}
-			//free allocated memory
-			free(dst);
+			phi_result = exec_scif_connect((scif_epd_t)cmd->int_args[0], (struct scif_portID *)cmd->extra_args[0].data, int_res);
 			break;
 		case ACCEPT:
 			printf("Executing scif_accept() ... \n");
--- src/process.h
+++ src/process.h
@@ -5,6 +5,14 @@ typedef struct client_node_s {
 	int id;
 } client_node;
 
+int exec_scif_open(scif_epd_t *endp, client_node **cur_client);
+int exec_scif_close(scif_epd_t *endp, client_node **cur_client);
+int exec_scif_bind(scif_epd_t endp, uint16_t pn, int *portno);
+int exec_scif_listen(scif_epd_t endp, int backlog);
+int exec_scif_connect(scif_epd_t endp, struct scif_portID *dst, int *portID);
+int exec_scif_accept(scif_epd_t endp, struct scif_portID *peer, scif_epd_t *newepd, int flags);
+
+
 int process_phi_cmd(void **result, void *cmd_ptr,client_node **cur_client);
 
 #endif /* PROCESS_H */
