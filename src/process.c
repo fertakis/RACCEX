ac1032250d9038e626bfdf5b4f9a02192077ebf8
Konstantinos <kfertak@cslab.ece.ntua.gr>
2018-01-13 21:24:02 +0200

    hmm ok doky

--- src/process.c
+++ src/process.c
@@ -581,17 +581,21 @@ int process_phi_cmd(void **result, void *cmd_ptr, client_node  **cur_client) {
 			//TODO: scif_get_node_ids call goes here...
 			arg_count += 2 ;
 
-			int len = cmd->int_args[0], online_nodes;
+			int len = cmd->int_args[0];
 			uint16_t *nodes, *self;
 
 			nodes = malloc_safe(sizeof(uint16_t) * len);
 			self = malloc_safe(sizeof(uint16_t));
-
-			phi_result = exec_scif_get_nodeIDs(nodes, len, self, &online_nodes);
-
-			extra_args = malloc_safe(sizeof(uint16_t)*(online_nodes+1));
-			memcpy(extra_args, nodes, sizeof(uint16_t)*online_nodes);
-			memcpy(extra_args+sizeof(uint16_t)*online_nodes, self, sizeof(uint16_t));
+			
+			int_res = malloc_safe(sizeof(int));
+			int_res_count = 1;
+				
+			phi_result = exec_scif_get_nodeIDs(nodes, len, self, int_res);
+			
+			extra_args_size = sizeof(uint16_t)*((*int_res)+1);
+			extra_args = malloc_safe(extra_args_size);
+			memcpy(extra_args, nodes, sizeof(uint16_t)*(*int_res));
+			memcpy(extra_args+sizeof(uint16_t)*(*int_res), self, sizeof(uint16_t));
 
 			break;
 		}
