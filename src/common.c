5df06f1222e5b356b4442800cc0d768e348315b9
Konstantinos Fertakis <kfertak@cslab.ece.ntua.gr>
2018-03-31 01:17:28 +0300

    asd

--- src/common.c
+++ src/common.c
@@ -252,12 +252,12 @@ void print_bytes(const void *object, size_t size)
 
 void print_ints(const void *object, size_t size)
 {
-	  const int *buff = (int *)object;
+	  int *buff = object;
 	  int i, len = size/sizeof(int);
 	  
 	  fprintf(out_fd, "[ ");
 	  for(i = 0; i < len; i++) {
-	  	fprintf(out_fd, "%d", buff[i]);
+	  	fprintf(out_fd, "%d ", buff[i]);
 	  	//fprintf(out_fd, "%d ", bytes[i]);
 	  }
 	  fprintf(out_fd, "]\n");
