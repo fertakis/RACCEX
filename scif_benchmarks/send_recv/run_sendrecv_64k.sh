5a693f90e9207ae36bb09c0f650d506e3c483835
Konstantinos Fertakis <kfertak@cslab.ece.ntua.gr>
2018-04-19 00:39:31 +0300

    micro sendrecv median

--- scif_benchmarks/send_recv/run_sendrecv_64k.sh
+++ scif_benchmarks/send_recv/run_sendrecv_64k.sh
@@ -4,8 +4,15 @@ i="1"
 
 while [ $i -lt 64000 ]
 do
-echo ./scif_send -r 1456 -s $i 
-./scif_send -r 1456 -s $i | tee results/run_rphi_$i.out
-i=$[$i*2]
-sleep 4
+	echo ./scif_send -r 1456 -s $i
+	val="1"
+	while [ $val -lt 11 ]
+	do
+		echo iteration $val
+		./scif_send -r 1456 -s $i | tee results/run_localhost_${i}_$val.out
+		val=$[${val} + 1]
+		sleep 4
+	done
+	i=$[$i*2]
+	sleep 4
 done
